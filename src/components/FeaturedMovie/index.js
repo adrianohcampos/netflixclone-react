24b972ac9e51f963c5a3dd5dbcd262059d07aae1
Adriano Campos <adrianohcampos@gmail.com>
2023-03-12 17:41:20 -0300

    Add btn more infos

--- src/components/FeaturedMovie/index.js
+++ src/components/FeaturedMovie/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+
 import './FeaturedMovie.css';
 
 const FeaturedMovie = ({ item }) => {
@@ -22,20 +25,21 @@ const FeaturedMovie = ({ item }) => {
   const truncatedDescription = getTruncatedDescription(overview);
   const releaseYear = new Date(first_air_date).getFullYear();
   const pluralSeasons = number_of_seasons !== 1 ? 's' : '';
-
+  let points = Math.round(vote_average * 10) / 10;
   return (
     <section className="featured" style={{ backgroundImage: `url(https://image.tmdb.org/t/p/original/${backdrop_path})` }}>
       <div className="featured--vertical">
         <div className="featured--horizontal">
           <div className="featured--name">{name}</div>
           <div className="featured--info">
-            <div className="featured--point">{vote_average} pontos</div>
+            <div className="featured--point">{points} pontos</div>
             <div className="featured--year">{releaseYear}</div>
             <div className="featured--seasons">{number_of_seasons} temporada{pluralSeasons}</div>
           </div>
           <div className="featured--description">{truncatedDescription}</div>
           <div className="featured--buttons">
-            <a href={`/watch/${id}`} className="featured--watchbutton">► Assistir</a>
+            <a href={`/watch/${id}`} className="featured--watchbutton"><PlayArrowIcon style={{ fontSize: 40 }} /> Assistir</a>
+            <a href={`/title/${id}`} className="featured--titlebutton"><ErrorOutlineIcon style={{ fontSize: 40 }} /> Mais Informações</a>
           </div>
         </div>
       </div>
